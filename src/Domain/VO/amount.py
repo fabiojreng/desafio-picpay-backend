b45c7f08eb533369350875bf84a93098d7a92738
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-05-01 20:19:53 -0300

    feat: Change amount implementation

--- src/Domain/VO/amount.py
+++ src/Domain/VO/amount.py
@@ -1,11 +1,8 @@
 class Amount:
     def __init__(self, amount: float) -> None:
-        self.__amount = amount
-
-    def create(self, amount):
         if amount < 0:
             raise ValueError("The balance cannot be negative")
-        return Amount(amount)
+        self.__amount = amount
 
     def get_value(self):
         return self.__amount
