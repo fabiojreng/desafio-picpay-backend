553334e27251b6f67307875f47e4f921619ece72
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-05-02 18:55:16 -0300

    feat: Implement http interface with flask

---
+++ src/Infra/Http/flask_adapter.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, json, Response
+from src.Infra.Http.interface_server_http import HttpServer
+
+
+class FlaskAdapter(HttpServer):
+    def __init__(self) -> None:
+        self.__app = Flask(__name__)
+
+    def register(self, method, url, callback):
+        view_name = f"{method.lower()}_{url.replace('/', '_')}_callback"
+
+        def flask_callback(*args, **kwargs):
+            try:
+                if request.method == "GET":
+                    if request.view_args:
+                        output = callback(request.view_args)
+                    else:
+                        output = callback(*args, **kwargs)
+
+                else:
+                    output = callback(request.json)
+                return (
+                    Response(
+                        json.dumps(output, sort_keys=False),
+                        mimetype="application/json",
+                    ),
+                    output["status_code"],
+                )
+            except Exception as e:
+                return (
+                    Response(
+                        json.dumps(
+                            {"status_code": 400, "body": str(e)}, sort_keys=False
+                        ),
+                        mimetype="application/json",
+                    ),
+                    400,
+                )
+
+        flask_callback.__name__ = view_name
+
+        return self.__app.add_url_rule(
+            rule=url, view_func=flask_callback, methods=[method.upper()]
+        )
+
+    def listen(self, port):
+        print(f"SERVER RUNNING IN PORT {port}")
+        return self.__app.run(port=port, debug=True)
