09f2bf38dc6373110a4a513a47ef952cc8d46ffe
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-05-03 10:36:30 -0300

    feat: remove bcrypt and use hashlib

--- src/Domain/VO/password.py
+++ src/Domain/VO/password.py
@@ -1,20 +1,21 @@
-import bcrypt
+import hashlib
 
 
 class Password:
     def __init__(self, password: str) -> None:
-        self.value = password
+        self.__value = password
 
     @staticmethod
     def create(password: str):
         password = password.replace(" ", "")
         if len(password) < 8:
             raise ValueError("Invalid password")
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-        return Password(hashed_password.decode("utf-8"))
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        return Password(hashed_password)
 
     def validate(self, password: str) -> bool:
-        return bcrypt.checkpw(password.encode("utf-8"), self.value.encode("utf-8"))
+        hashed_input = hashlib.sha256(password.encode()).hexdigest()
+        return hashed_input == self.__value
 
     def get_value(self) -> str:
-        return self.value
+        return self.__value
