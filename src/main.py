fa3c8f59bbb0f892c517d9df4880ce174b7648d1
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-05-03 13:22:31 -0300

    feat: Create file to manage application instances

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os, sys
+
+sys.path.insert(0, os.path.abspath(os.curdir))
+
+from src.Application.UseCases.create_transaction import CreateTransactionUseCase
+from src.Application.UseCases.deposit_amount import DepositAmountUseCase
+from src.Application.UseCases.create_user import CreateUserUseCase
+from src.Infra.DataBase.connection_mysql import ConnectionMySql
+from src.Infra.Http.flask_adapter import FlaskAdapter
+from src.Infra.Repository.MySQL.user_repository_mysql import UserRepositoryMySQL
+from src.Infra.Controller.main_controller import MainController
+from src.Infra.Repository.MySQL.transaction_repository_mysql import (
+    TransactionRepositoryMySQL,
+)
+
+server = FlaskAdapter()
+connection = ConnectionMySql()
+db_user = UserRepositoryMySQL(connection)
+db_transaction = TransactionRepositoryMySQL(connection)
+
+create_user = CreateUserUseCase(db_user)
+deposit_amount = DepositAmountUseCase(db_user)
+transaction = CreateTransactionUseCase(db_transaction, db_user)
+
+MainController(server, create_user, deposit_amount, transaction)
+server.listen(3333)
