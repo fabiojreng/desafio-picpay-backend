1399cbd3e85866f406dad86e55cd78fff0435943
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-05-04 16:08:50 -0300

    feat: Create the route to return all transactions

--- src/Infra/Controller/main_controller.py
+++ src/Infra/Controller/main_controller.py
@@ -9,14 +9,17 @@ class MainController:
         create_user: UseCaseInterface,
         deposit_amount: UseCaseInterface,
         create_transaction: UseCaseInterface,
+        find_all_transactions: UseCaseInterface,
     ) -> None:
         self.__create_user_use_case = create_user
         self.__deposit_amount_use_case = deposit_amount
         self.__create_transaction_use_case = create_transaction
+        self.__find_all_transactions = find_all_transactions
         http_server.register("get", "/", self.__server)
         http_server.register("post", "/create-user", self.__create_user)
         http_server.register("post", "/deposit-amount", self.__deposit_amount)
         http_server.register("post", "/transaction", self.__create_transaction)
+        http_server.register("get", "/transactions", self.__transactions)
 
     def __server(self):
         return {"status_code": 200, "body": {"message": "Bem Vindo"}}
@@ -31,4 +34,8 @@ class MainController:
 
     def __create_transaction(self, req):
         output = self.__create_transaction_use_case.execute(req)
+        return output
+
+    def __transactions(self):
+        output = self.__find_all_transactions.execute()
         return output
