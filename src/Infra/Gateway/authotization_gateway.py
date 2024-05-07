6f30b644e8c6220e6ba7d799505331102543bc22
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-05-06 21:57:37 -0300

    feat: Implement the authorization gateway interface

---
+++ src/Infra/Gateway/authotization_gateway.py
@@ -0,0 +1,13 @@
+from src.Domain.Gateway.autorization_gateway import AuthorizationGatewayInterface
+from src.Infra.Http.interface_client_http import HttpClientInterface
+
+
+class AuthorizationGateway(AuthorizationGatewayInterface):
+    def __init__(self, http_client: HttpClientInterface) -> None:
+        self.__htpp_client = http_client
+
+    def authorization(self):
+        # return {"status": 200, "data": {"message": "Não autorizado"}}
+        return self.__htpp_client.get(
+            "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc"
+        )
